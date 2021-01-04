85405a7be4b836abeda49d2333ef3c3158ec92d8
Berkhan122 <berkhantapan0665@gmail.com>
2021-01-04 22:39:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,91 +10,62 @@ namespace SayiTahminOyunu
     {
         static void Main(string[] args)
         {
-        Baslangic:
             Console.WriteLine("Sayı Tahmin Oyunununa Hoş Geldiniz\n");
-        Hata:
-            Console.Write("1-3 Arası Zorluk Seviyesi Seçiniz:");
-            int zorlukseviyesi = int.Parse(Console.ReadLine());
-            if (zorlukseviyesi > 3)
-            {
-                Console.WriteLine("Lütfen 1-3 arası zorluk seviyesi giriniz");
-                goto Hata;
-            }
-            int kalanhak = 0;
             Random rnd = new Random();
+            byte baslangic = 0;
+            byte bitis = 0;
+            byte zorlukseviyesi = 0;
+            byte kalanhak = 0;
+            int sayi2 = 0;
+            do
+            {
+                Console.Write("1-3 Arası Zorluk Seviyesi Seçiniz:");
+                zorlukseviyesi = byte.Parse(Console.ReadLine());
+
+            } while (zorlukseviyesi != 1 && zorlukseviyesi != 2 && zorlukseviyesi != 3);
 
             switch (zorlukseviyesi)
             {
                 case 1:
-                    kalanhak = 6;
+                    baslangic = 1;
+                    bitis = 10;
                     Console.WriteLine("1. Seviye Zorluk Seçtiniz");
-                    int sayi1 = rnd.Next(1, 10);
-                    for (; ; )
-                    {
-                        Console.WriteLine("Kalan hakkınız:" + kalanhak);
-                        Console.WriteLine("Bir sayı giriniz:");
-                        int sayi = int.Parse(Console.ReadLine());
-                        if (sayi == sayi1)
-                        {
-                            Console.WriteLine("Tebrikler");
-                            goto Baslangic;
-                        }
-                        if (kalanhak <= 1)
-                        {
-                            Console.WriteLine("Deneme Hakkınız Bitmiştir.");
-                            goto Baslangic;
-                        }
-                        kalanhak--;
-                    }
-
+                    kalanhak = 6;
+                    sayi2 = rnd.Next(baslangic,bitis);
+                    break;
                 case 2:
-                    kalanhak = 4;
+                    baslangic = 1;
+                    bitis = 25;
                     Console.WriteLine("2. Seviye Zorluk Seçtiniz");
-
-                    int sayi2 = rnd.Next(1, 25);
-                    for (; ; )
-                    {
-                        Console.WriteLine("Kalan hakkınız:" + kalanhak);
-                        Console.WriteLine("Bir sayı giriniz:");
-                        int sayi = int.Parse(Console.ReadLine());
-                        if (sayi == sayi2)
-                        {
-                            Console.WriteLine("Tebrikler");
-                            goto Baslangic;
-                        }
-                        if (kalanhak <= 1)
-                        {
-                            Console.WriteLine("Deneme Hakkınız Bitmiştir.");
-                            goto Baslangic;
-                        }
-                        kalanhak--;
-                    }
-
+                    kalanhak = 4;
+                    sayi2 = rnd.Next(baslangic, bitis);
+                    break;
                 case 3:
-                    kalanhak = 2;
+                    baslangic = 1;
+                    bitis = 50;
                     Console.WriteLine("3. Seviye Zorluk Seçtiniz");
+                    kalanhak = 2;
+                    sayi2 = rnd.Next(baslangic, bitis);
+                    break;
+            }
 
-                    int sayi3 = rnd.Next(1, 50);
-                    for (; ; )
-                    {
-                        Console.WriteLine("Kalan hakkınız:" + kalanhak);
-                        Console.WriteLine("Bir sayı giriniz:");
-                        int sayi = int.Parse(Console.ReadLine());
-                        if (sayi == sayi3)
-                        {
-                            Console.WriteLine("Tebrikler");
-                            goto Baslangic;
-                        }
-                        if (kalanhak <= 1)
-                        {
-                            Console.WriteLine("Deneme Hakkınız Bitmiştir.");
-                            goto Baslangic;
-                        }
-                        kalanhak--;
-                    }
+            for (; ; )
+            {
+                Console.WriteLine("Kalan hakkınız:" + kalanhak);
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi == sayi2)
+                {
+                    Console.WriteLine("Tebrikler");
+                }
+                if (kalanhak <= 1)
+                {
+                    Console.WriteLine("Deneme Hakkınız Bitmiştir.");
+                    break;
+                }
+                kalanhak--;
             }
         }
     }
 
 }
-
